7971b062720d8a6c928bdc26038054bc9587252f
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-27 17:31:01 +0530

    Private/ Public Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Switch } from "react-router";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import PrivateRoute from "./Components/PrivateRoute";
+import PublicRoute from "./Components/PublicRoutes";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <PublicRoute>
+          <SignIn />
+        </PublicRoute>
+
+        <PrivateRoute>
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
